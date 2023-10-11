1076ef48d8cc83eddd47c44c9be1f84893b636d9
GeorgiiMalishev <malishev292@gmail.com>
2023-10-11 23:18:00 +0500

    Update task2.py

--- buns/mod3/task2.py
+++ buns/mod3/task2.py
@@ -1,2 +1,2 @@
 num = int(input())
-print("Неверный ввод" if num < 0 else " ".join([bin(num), oct(num), hex(num)]))
+print("Неверный ввод" if num <= 0 else ", ".join([bin(num)[2:], oct(num)[2:], hex(num)[2:]]))
